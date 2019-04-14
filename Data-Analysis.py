51eca6243af46f84aa808a109dd1b2ef9433de59
Emmi Carr <escarr@umich.edu>
2019-04-14 14:39:59 -0400

    Getting categories from Yelp

--- Data-Analysis.py
+++ Data-Analysis.py
@@ -1,1 +1,12 @@
 # analysis
+import sqlite3
+conn = sqlite3.connect('rest_data.sqlite')
+cur = conn.cursor()
+
+#number of restaurants in each category
+cur.execute("SELECT category FROM Yelp")
+count = 0
+for row in cur:
+    print(row[0])
+    count = count + 1
+print(count)
