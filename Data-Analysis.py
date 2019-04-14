9511dac5214274fce201bfb0c44f03fd7be812eb
Emmi Carr <escarr@umich.edu>
2019-04-14 14:55:08 -0400

    Get number of restaurants in each category

--- Data-Analysis.py
+++ Data-Analysis.py
@@ -1,12 +1,26 @@
 # analysis
 import sqlite3
+import json
 conn = sqlite3.connect('rest_data.sqlite')
 cur = conn.cursor()
 
 #number of restaurants in each category
+#YELP
 cur.execute("SELECT category FROM Yelp")
-count = 0
+categories_yelp = dict()
 for row in cur:
-    print(row[0])
-    count = count + 1
-print(count)
+    cat = row[0]
+    categories_yelp[cat] = categories_yelp.get(cat, 0) + 1
+
+
+print(sorted(categories_yelp.items(), key = lambda t:t[1], reverse = True))
+
+#ZOMATO
+cur.execute("SELECT category FROM Zomato")
+categories_zomato = dict()
+for row in cur:
+    cat = row[0]
+    categories_zomato[cat] = categories_zomato.get(cat, 0) + 1
+
+
+print(sorted(categories_zomato.items(), key = lambda t:t[1], reverse = True))
