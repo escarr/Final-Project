3cbb3e54cf6666231f1a63985ca2439b7aac14fb
Madeline Richards <madeline@0587353149.wireless.umich.net>
2019-04-14 15:27:18 -0400

    Avg Star Rating by Category

--- Data-Analysis.py
+++ Data-Analysis.py
@@ -13,7 +13,8 @@ for row in cur:
     categories_yelp[cat] = categories_yelp.get(cat, 0) + 1
 
 
-print(sorted(categories_yelp.items(), key = lambda t:t[1], reverse = True))
+results_y = sorted(categories_yelp.items(), key = lambda t:t[1], reverse = True)
+print(results_y)
 
 #ZOMATO
 cur.execute("SELECT category FROM Zomato")
@@ -23,4 +24,57 @@ for row in cur:
     categories_zomato[cat] = categories_zomato.get(cat, 0) + 1
 
 
-print(sorted(categories_zomato.items(), key = lambda t:t[1], reverse = True))
+results_z = sorted(categories_zomato.items(), key = lambda t:t[1], reverse = True)
+print(results_z)
+
+#Ave Star Rating by Category
+
+#YELP
+
+print('------------Average Star Rating by Category------------')
+
+print('--------YELP----------')
+
+
+cur.execute("SELECT category, rating FROM Yelp")
+rating_by_cat_y = {}
+for row in cur:
+    cat = row[0]
+    rating = row[1]
+    if cat in rating_by_cat_y:
+        rating_by_cat_y[cat].append(rating)
+    else:
+        rating_by_cat_y[cat] = [rating]
+
+print (rating_by_cat_y)
+
+for cat in rating_by_cat_y:
+    avg = sum(rating_by_cat_y[cat])/len(rating_by_cat_y[cat])
+    rounded = round(avg, 2)
+    print(cat + ': ' + str(rounded))
+  
+
+
+#ZOMATO
+
+print('--------ZOMATO----------')
+
+cur.execute("SELECT category, rating FROM Zomato")
+rating_by_cat_z = {}
+for row in cur:
+    cat = row[0]
+    rating = row[1]
+    if cat in rating_by_cat_z:
+        rating_by_cat_z[cat].append(rating)
+    else:
+        rating_by_cat_z[cat] = [rating]
+
+
+
+for cat in rating_by_cat_z:
+    avg = sum(rating_by_cat_z[cat])/len(rating_by_cat_z[cat])
+    rounded = round(avg, 2)
+    print(cat + ': ' + str(rounded))
+  
+    
+
